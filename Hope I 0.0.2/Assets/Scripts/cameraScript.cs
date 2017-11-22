38bec0405c3dffe417d22b5f7b2c434658ca5c71
Mohamed <mohamed.kalia@esprit.tn>
2017-11-22 23:24:56 +0100

    scrolling
    new assets + parallax scrolling

---
+++ Hope I 0.0.2/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp((new Vector3 (transform.position.x, transform.position.y, transform.position.z)), (new Vector3(Mathf.Clamp(desiredPosition.x, -17.0F, 30.0F), Mathf.Clamp(desiredPosition.y, 0.3F, 1.3F), transform.position.z)), smoothSpeed);
+        transform.position = smoothedPosition;
+
+        //transform.LookAt(target);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
